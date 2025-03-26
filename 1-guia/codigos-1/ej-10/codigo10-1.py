ac0a24a441f30bc4995521657369467b2d783eb8
nad-garraz <22796744+nad-garraz@users.noreply.github.com>
2025-03-26 15:50:51 -0300

    add ejercicio 10 guia 1

--- 1-guia/codigos-1/ej-10/codigo10-1.py
+++ 1-guia/codigos-1/ej-10/codigo10-1.py
@@ -1,12 +1,13 @@
 import numpy as np
 
 # Matriz del ejercicio
-
-A = [[1, 4, -1, 3], [2, 1, -3, 1], [0, 2, 1, -5, 1]]
-b = [0, 0, 0, 0]
+A = np.array([[1-1j, 2], [1j, -1 + 1j]])
+b = [0, 0]
 
 print(A)
 
 # Resuelvo el sistema A x = b, y lo devuelvo en
 # las variables con los nombres adecuados
-a1, b1, c1 = np.linalg.solve(A, b)
+alpha, beta = np.linalg.solve(A, b)
+
+print(f"alpha = {alpha}\nbeta = {beta}")
