9752289c9cdca09fd1eef26727553eee77b774ef
nad-garraz <22796744+nad-garraz@users.noreply.github.com>
2025-06-05 11:35:33 -0300

    add ej 7 guia 5 remasterizado

--- 5-guia/codigos-5/ej-7/codigo7-1.py
+++ 5-guia/codigos-5/ej-7/codigo7-1.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-#
-# A = np.array([[4, 0], [3, 5]])
+
+A = np.array([[4, 0], [3, 5]])
+A_inv = 0.05 * np.array([[5, 0], [-3, 4]]) 
 # H = np.transpose(A) @ A
 # # H2 = A @ np.transpose(A)
 #
@@ -14,30 +15,42 @@ import matplotlib.pyplot as plt
 # print(A @ v2)
 
 # Nuestra Matriz
-M = np.array([[4, 0], [3, 5]])
-V = np.random.rand(2, 200) - 0.5
+V = np.random.rand(2, 200) - 0.5 # circunferencia unitaria
 V_uni = V / np.linalg.norm(V, axis=0)
+AV_uni = A @ V_uni
+A_invV_uni = A_inv @ V_uni
+print(np.linalg.svd(A_inv))
 
-# Genero data para poder hacer el gráfico en TiKz
-np.savetxt(
-    f"../../ejercicios-5/dataFiles/7-ej-data/item-circulo.data",
-    np.transpose([V_uni[0], V_uni[1]]),
-    fmt="%.10e",
-    header="circulo y producto por matriz",
-    comments="# SVD circulo unitario",
-)
+## Genero data para poder hacer el gráfico en TiKz
+## -----------------------------------------------
+# np.savetxt(
+#     f"../../ejercicios-5/dataFiles/7-ej-data/item-circulo.data",
+#     np.transpose([V_uni[0], V_uni[1]]),
+#     fmt="%.10e",
+#     header="circulo y producto por matriz",
+#     comments="# SVD circulo unitario",
+# )
+#
+# np.savetxt( # matriz A
+#     f"../../ejercicios-5/dataFiles/7-ej-data/item-matrizDotCirculo.data",
+#     np.transpose([AV_uni[0], AV_uni[1]]),
+#     fmt="%.10e",
+#     header="circulo y producto por matriz",
+#     comments="# SVD Matriz por circulo unitario",
+# )
+# np.savetxt( # matriz A inversa
+#     f"../../ejercicios-5/dataFiles/7-ej-data/item-matrizInvDotCirculo.data",
+#     np.transpose([A_invV_uni[0], A_invV_uni[1]]),
+#     fmt="%.10e",
+#     header="circulo y producto por matriz inversa",
+#     comments="# SVD Matriz inversa por circulo unitario",
+# )
+## -----------------------------------------------
 
-np.savetxt(
-    f"../../ejercicios-5/dataFiles/7-ej-data/item-matrizDotCirculo.data",
-    np.transpose([MV_uni[0], MV_uni[1]]),
-    fmt="%.10e",
-    header="circulo y producto por matriz",
-    comments="# SVD Matriz por circulo unitario",
-)
 # Ploteo dos figuras, una para radio 1 y otra para radio 3
 fig, (ax1) = plt.subplots(1, 1, figsize=(12, 5))
 ax1.scatter(V_uni[0], V_uni[1], label="v_uni")
-ax1.scatter(MV_uni[0], MV_uni[1], label="Av_uni")
+ax1.scatter(AV_uni[0], AV_uni[1], label="Av_uni")
 ax1.legend()
 ax1.grid()
 ax1.set_aspect("equal")  # aspect ratio para que se vea cuadradito
